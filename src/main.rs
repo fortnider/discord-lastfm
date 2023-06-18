3814e8d03772b0c93ecbdd2ceaa982d4e1c11da5
fortnider <max.a.xue@protonmail.com>
2023-06-18 01:37:18 -0700

    auto reconnect wiht exponential backoff.... i think... maybe buggy

--- src/main.rs
+++ src/main.rs
@@ -37,80 +37,68 @@ async fn last_fm_poll(lastfm_tx: Sender<String>){
     let mut last_fm_user:String = env::var("LAST_FM_USER").unwrap();
     let mut last_fm_url: String = format!("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={}&api_key={}&limit=1&format=json", last_fm_user, last_fm_api_key);
     let mut poll_time: u64 = 10;
-    let mut status = "online"; // "online", "dnd", "idle"
+    let status = "online"; // "online", "dnd", "idle"
     // In the future will be editable using GUI, so keeping it mut. 
 
     let mut lfm_response:Value;
     let mut song: Vec<String> = vec!["".to_string(),"".to_string(),"".to_string()];
     let mut status_update:String;
     let mut status_cleared = true;
-    let mut lfm_query:Result<reqwest::Response, reqwest::Error>;
     // Variables for later
 
     let backoff_timer = vec![10, 20, 30, 60, 120, 300, 600];
-    let mut backoff_count: usize = 0;
-
+    let mut lfm_backoff_count: usize = 0;
+    info!("fdsjklsdfjlj");
     loop{
-        loop{
-            lfm_query = reqwest::get(&last_fm_url).await;
-            match lfm_query{
+        'lfm_query: loop{
+            match reqwest::get(&last_fm_url).await{
                 Ok(response) => {
+                    info!("last.fm Response Received");
                     lfm_response = response.json().await.expect("Could not parse json");
-                    break
+                    break 'lfm_query;
                 },
-                Err(_) => ()
-    
+                Err(_) => {
+                    sleep(Duration::from_secs(*backoff_timer.get(lfm_backoff_count).unwrap_or(&600))).await;
+                    error!("Could not connect to last.fm! Waiting {} seconds before retrying.",*backoff_timer.get(lfm_backoff_count).unwrap_or(&600));
+                    lfm_backoff_count += 1;
+                }
             };
-
-            sleep(Duration::from_secs(*backoff_timer.get(backoff_count).unwrap_or(&600))).await;
-            error!("Could not connect to last.fm! Waiting {} seconds before retrying.",*backoff_timer.get(backoff_count).unwrap_or(&600));
-            backoff_count += 1;
         }
+        lfm_backoff_count = 0;
 
-        //debug!("LastFM Returned {:#?}", lfm_response);
-        if lfm_response["recenttracks"]["track"][0]["@attr"]["nowplaying"].to_string() == r#""true""#
+        let now_playing = lfm_response["recenttracks"]["track"][0]["@attr"]["nowplaying"].as_str().unwrap_or_default();
+        let song_name = lfm_response.pointer("/recenttracks/track/0/name").unwrap().to_string();
+        let song_artist = lfm_response.pointer("/recenttracks/track/0/artist/#text").unwrap().to_string();
+        let song_album = lfm_response.pointer("/recenttracks/track/0/album/#text").unwrap().to_string();
+
+        if now_playing == "true"
         // If the song is now playing
         {
-
-            if lfm_response.pointer("/recenttracks/track/0/name").unwrap().to_string() != song[0] 
-            || lfm_response.pointer("/recenttracks/track/0/artist/#text").unwrap().to_string() != song[1]
-            || lfm_response.pointer("/recenttracks/track/0/album/#text").unwrap().to_string() != song[2]{
+            if song_name != song[0] || song_artist != song[1] || song_album != song[2]{
                 // If song is different than previous song
-
                 info!("Song is currently playing and different. Updating status.");
-                song= vec![
-                lfm_response.pointer("/recenttracks/track/0/name").unwrap().to_string(),
-                lfm_response.pointer("/recenttracks/track/0/artist/#text").unwrap().to_string(),
-                lfm_response.pointer("/recenttracks/track/0/album/#text").unwrap().to_string(),
-                ];
+                song= vec![song_name, song_artist, song_album];
     
-    
+
                 status_update = r#"{"op": 3, "d": {"since": 0,"activities": [{"name": "NAME","type": 2,"id": "custom", "details":"DETAILS"}],"status": "STATUS","afk": false}}"#
-                // ,"status": "STATUS","afk": false
-                .replace("STATUS", status)
-                .replace("NAME", format!("{} - {}", &song[0], &song[1]).replace(r#"""#, "").as_str())
-                .replace("DETAILS", format!("From {}", &song[2]).replace(r#"""#, "").as_str());
+                    .replace("STATUS", status)
+                    .replace("NAME", format!("{} - {}", &song[0], &song[1]).replace(r#"""#, "").as_str())
+                    .replace("DETAILS", format!("From {}", &song[2]).replace(r#"""#, "").as_str());
                 lastfm_tx.send(status_update.to_string()).await.expect("couldnt send status update"); 
                 status_cleared = false;
-            
-            
             }
             
             
         }
         else if status_cleared == false
-        // If song is not now playing
+        // If song is not now playing and status has not been cleared
             {
-                debug!("Song is not currently playing and status has not been cleared. Clearing status now. ");
+                info!("Song is not currently playing and status has not been cleared. Clearing status now. ");
                 status_update = r#"{"op": 3, "d": {"since": 0,"activities": null,"status": "STATUS","afk": false}}"#.to_string();
                 lastfm_tx.send(status_update.to_string()).await.expect("couldnt send status update"); 
                 status_cleared = true;
 
             }
-        else {
-            //debug!("Song is not currently playing.");
-        }
-    
     sleep(Duration::from_secs(poll_time)).await;
 
     }
@@ -199,8 +187,7 @@ async fn heartbeat(heartbeat_tx: Sender<String>, heartbeat_interval: u64, mut he
         debug!("Heartbeat Sent");
         sleep(Duration::from_millis(heartbeat_interval)).await;
         if heartbeat_ack_rx.try_recv().unwrap_or(false) == false {
-            warn!("No heartbeat detected! Returning");
-            // NEED TO FIX!! WHEN THIS RETURNS THE PROGRAM TERMINATES
+            warn!("No heartbeat detected! Heartbeat function returning");
             return
 
         };
@@ -218,33 +205,55 @@ async fn main(){
     let token:String = env::var("DISCORD_TOKEN").unwrap();
 
     let status = "online"; // "online", "dnd", "idle"
-
-    
     let mut res:Result<Response, Error>;
+
+    let mut heartbeat_interval: u64; 
+
+    let mut ws_stream;
 
     loop {
         loop{
+            info!("Attempting to connect to Discord");
+
             res = client
             .get("https://discordapp.com/api/v9/users/@me")
             .header("Authorization", &token)
             .send()
             .await;
-
+            
             match &res {
                 Ok(resp) => {
                     if resp.status().to_string() == "200 OK"{
                         info!("Token Valid");
+                        (ws_stream, _) = connect_async("wss://gateway.discord.gg/?v=9&encoding=json").await.expect("Failed ws connect");
+            
+                        let hello: Value = serde_json::from_str(&ws_stream.next().await.unwrap().unwrap().to_string()).unwrap();
+                        // Wait until hello from discord is recieved, then turn into json. 
+                        info!("Received hello: {:#?}", hello );
+                
+                        heartbeat_interval = hello.pointer("/d/heartbeat_interval").unwrap().to_string().parse::<u64>().unwrap();
+                        info!("Heartbeat: {:?}", heartbeat_interval);
+                        // Retrieve heartbeat from json. 
+                
+                        let identity = r#"{"op": 2, "d": {"token": "TOKEN", "properties": {"$os": "Windows 10", "$browser": "Google Chrome", "$device": "Windows"}, "presence": {"status": "STATUS", "afk": false}}, "s": null, "t": null}"#
+                            .replace("TOKEN", &token)
+                            .replace("STATUS", &status);
+                
+                        ws_stream.send(Message::Text(identity.to_string())).await.expect("couldn't send identity... le sigh");
+                        info!("Successfully connected to Discord");
+
+
                         break
-                    }
-                    else {
-                        warn!("Token Invalid or other error! \n{}", resp.status());
+                        }
+                        else {
+                            error!("Token Invalid or other error! \n{}", resp.status());
                         
                     };
                     
                 },
 
                 Err(_) => {
-                    error!("Could not connect to last.fm! Waiting {} seconds before retrying.",*backoff_timer.get(backoff_count).unwrap_or(&600));
+                    error!("Could not connect to Discord! Waiting {} seconds before retrying.",*backoff_timer.get(backoff_count).unwrap_or(&600));
                 }
             };
 
@@ -257,23 +266,7 @@ async fn main(){
 
         //Connect to discord using token. If it fails, print error. Otherwise, token is valid. 
 
-        let (mut ws_stream, _) = 
-            connect_async("wss://gateway.discord.gg/?v=9&encoding=json").await.expect("Failed ws connect");
 
-        let hello: Value = serde_json::from_str(&ws_stream.next().await.unwrap().unwrap().to_string()).unwrap();
-        // Wait until hello from discord is recieved, then turn into json. 
-        info!("Received hello: {:#?}", hello );
-
-        let heartbeat_interval: u64 = hello.pointer("/d/heartbeat_interval").unwrap().to_string().parse::<u64>().unwrap();
-        info!("Heartbeat: {:?}", heartbeat_interval);
-        // Retrieve heartbeat from json. 
-
-        let identity = r#"{"op": 2, "d": {"token": "TOKEN", "properties": {"$os": "Windows 10", "$browser": "Google Chrome", "$device": "Windows"}, "presence": {"status": "STATUS", "afk": false}}, "s": null, "t": null}"#
-            .replace("TOKEN", &token)
-            .replace("STATUS", &status);
-
-        ws_stream.send(Message::Text(identity.to_string())).await.expect("couldn't send identity... le sigh");
-        info!("Sent Identity: {:#?}", identity);
 
 
         let (heartbeat_tx, mut rx) = mpsc::channel::<String>(3); 
